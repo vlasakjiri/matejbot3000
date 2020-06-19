bc701a58954c27135807f2bbeeb42c076cc343c4
Jiří Vlasák <jirivlasak9@gmail.com>
2020-06-19 20:21:25 +0200

    fixed buf

--- commands/vypadni.js
+++ commands/vypadni.js
@@ -2,12 +2,9 @@ module.exports = {
     name: 'vypadni',
     description: 'Vypadne z voice chanellu',
     execute(msg, args) {
-        if (msg.client.voice.channel) {
+        if (msg.member.voice.channel) {
             msg.channel.send("Tak si to tady fakt užijte.");
             msg.member.voice.channel.leave();
-        }
-        else {
-            msg.channel.send("Dej mi pokoj :( už nemám kam vypadnout :(");
         }
     },
 };
